2dcc06615e80c5b5162e99ecd0ff7d2a3f19f5f4
Madhur prashant <Madhurpt@amazon.com>
2024-04-03 10:49:55 -0400

    new changes to metrics, bedrock additions

--- src/fmbench/scripts/bedrock_predictor.py
+++ src/fmbench/scripts/bedrock_predictor.py
@@ -2,29 +2,34 @@ import os
 import copy
 import boto3
 import logging
-from litellm import embedding ## import litellm for bedrock model support: embedding models in this case
-from litellm import completion ## support for text generation models on bedrock
+from litellm import embedding
+from litellm import completion 
 from typing import Dict, Optional
 from fmbench.scripts.fmbench_predictor import FMBenchPredictor, FMBenchPredictionResponse
 
-## set a logger
+# set a logger
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-## represents the current bedrock embedding models
-EMBEDDING_MODELS: list[str] = ["amazon.titan-embed-text-v1", "cohere.embed-english-v3", "cohere.embed-multilingual-v3"] 
+# Initialize the global variables, including the embeddings model declaration and the service name
+global_vars = globals()
+global_vars['EMBEDDING_MODELS']: list[str] = ["amazon.titan-embed-text-v1", "cohere.embed-english-v3", "cohere.embed-multilingual-v3"]  # type: ignore
+global_vars['SERVICE_NAME']: str = 'bedrock'
 
-## Represents the class to predict using a bedrock rest API 
+# Represents the class to predict using a bedrock rest API 
 class BedrockPredictor(FMBenchPredictor):
-    ## initialize the service name
-    service_name: str = "bedrock"
+
+    # initialize the service name
+    service_name: str = global_vars['SERVICE_NAME']
+
     # overriding abstract method
     def __init__(self, endpoint_name: str, inference_spec: Dict | None):
-        try: ## initializing some of the variables in the contructor 
+        try:  
             self._endpoint_name = endpoint_name
             self._inference_spec = inference_spec
             self._predictor = boto3.client('bedrock-runtime')
             self.aws_region = boto3.Session().region_name
+            # this is used to invoke the litellm API with the specific bedrock model offering of choice
             self.bedrock_model = f"{self.service_name}/{self.endpoint_name}"
             self.response_json = {}
             logger.info(f"__init__ self._predictor={self._predictor}")
@@ -35,32 +40,35 @@ class BedrockPredictor(FMBenchPredictor):
     def get_prediction(self, payload: Dict) -> FMBenchPredictionResponse:
         # Represents the prompt payload
         prompt_input_data = payload['inputs']
+        # Represents the inference parameters (in this case, temperature and caching) 
         parameters = copy.deepcopy(payload['parameters'])
-        # get the temperature and caching for invoking the bedrock model via the lite llm api
+
+        # get the temperature, max_tokens and caching values as inference parameters 
         temperature = parameters.get('temperature', None)
+        max_tokens = parameters.get('max_tokens', None)
         caching = parameters.get('caching', None)
-        logger.info(f"temperature value from the inference parameters is: {temperature}, and caching is set to {caching}")
+        logger.info(f"Temperature passed for bedrock invocation: {temperature}, Max tokens: {max_tokens}, Caching: {caching}... ")
+
         os.environ["AWS_REGION_NAME"] = self.aws_region
         try:
-            ## Represents calling the litellm completion/messaging api utilizing the completion/embeddings API
-            ## [CLAUDE, LLAMA, ai21, MISTRAL, MIXTRAL, COHERE]
+            # this response is for text generation models on bedrock: CLAUDE, LLAMA, ai21, MISTRAL, MIXTRAL, COHERE
             logger.info(f"Invoking {self.bedrock_model} to get inference....")
             response = completion(
             model=self.bedrock_model,
             temperature = temperature,
+            max_tokens = max_tokens,
             caching = caching,
             messages=[{ "content": prompt_input_data,"role": "user"}], 
             )
-
-            ## iterate through the entire model response
+            # iterate through the entire model response
             for choice in response.choices:
-                ## extract the message and the message's content from litellm
+                # extract the message and the message's content from litellm
                 if choice.message and choice.message.content:
-                    ## extract the response from the dict
+                    # extract the response from the dict
                     self.response_json["generated_text"] = choice.message.content
                     break
 
-            # Extract number of input and completion prompt tokens (this is the same structure for embeddings and text generation models on Amazon Bedrock)
+            # Extract number of input and completion prompt tokens
             prompt_tokens = response.usage.prompt_tokens
             completion_tokens = response.usage.completion_tokens
             # Extract latency in seconds
@@ -70,24 +78,25 @@ class BedrockPredictor(FMBenchPredictor):
             logger.error(f"Exception occurred during prediction for endpoint_name={self._endpoint_name}, exception={e}")
         return FMBenchPredictionResponse(response_json=self.response_json, latency=latency, completion_tokens=completion_tokens, prompt_tokens=prompt_tokens)
 
-    def calculate_cost(self, instance_type, config: dict, duration: float, metrics: dict) -> float:
+    def calculate_cost(self, instance_type: str, config: dict, duration: float, metrics: dict) -> float:
         """Represents the function to calculate the cost for Bedrock experiments."""
-        experiment_cost: Optional[float] = 0.0 ## not changing into None since int can only be added, not Nonetype
+        # Initializing all cost variables
+        experiment_cost: Optional[float] = 0.0  
+        input_token_cost: Optional[float] = 0.0
+        output_token_cost: Optional[float] = 0.0
         try:
             if metrics:
-                prompt_tokens = metrics.get("all_prompts_token_count", )
-                completion_tokens = metrics.get("all_completions_token_count", )
+                prompt_tokens = metrics.get("all_prompts_token_count", None)
+                completion_tokens = metrics.get("all_completions_token_count", None)
                 # Retrieve the pricing information for the instance type
                 instance_pricing = config['pricing'].get(instance_type, []) 
-                logger.info(f"pricing dict: {instance_pricing}")
+                logger.info(f"Instance pricing for {instance_type}: {instance_pricing}")
                 # Calculate cost based on the number of input and output tokens
-                input_token_cost = 0.0
-                output_token_cost = 0.0
                 for pricing in instance_pricing:
                     input_token_cost += (prompt_tokens / 1000.0) * pricing.get('input-per-1k-tokens', 0)
                     output_token_cost += (completion_tokens / 1000.0) * pricing.get('output-per-1k-tokens', 0)
-                logger.info(f"input per 1k token pricing: {input_token_cost}")
-                logger.info(f"output per 1k token pricing: {output_token_cost}")
+                logger.info(f"input token cost: {input_token_cost}")
+                logger.info(f"output token cost: {output_token_cost}")
                 experiment_cost = input_token_cost + output_token_cost
         except Exception as e:
             logger.error(f"Exception occurred during experiment cost calculation....., exception={e}")
@@ -98,17 +107,25 @@ class BedrockPredictor(FMBenchPredictor):
         """The endpoint name property."""
         return self._endpoint_name
     
-## subclass of BedrockPredictor for embedding models supported on Amazon Bedrock
+# Subclass of BedrockPredictor for embedding models supported on Amazon Bedrock
 class BedrockPredictorEmbeddings(BedrockPredictor):
     def get_prediction(self, payload: Dict) -> FMBenchPredictionResponse:
-        ## Represents the prompt payload
+        # Represents the prompt payload
         prompt_input_data = payload['inputs'] 
-        ## getting the aws account region as an environment variable as declared in the litellm documentation
+        # Represents the inference parameters (in this case, temperature and caching) 
+        parameters = copy.deepcopy(payload['parameters'])
+        # get the temperature, max_tokens and caching values as inference parameters 
+        temperature = parameters.get('temperature', None)
+        max_tokens = parameters.get('max_tokens', None)
+        caching = parameters.get('caching', None)
         os.environ["AWS_REGION_NAME"] = self.aws_region
         try:
-            logger.info(f"Invoking {self.bedrock_model} to get inference....")
+            logger.info(f"Invoking {self.bedrock_model} embeddings model to get inference....")
             response = embedding(
                 model=self.bedrock_model,
+                temperature = temperature,
+                max_tokens = max_tokens,
+                caching = caching,
                 input=[prompt_input_data],
             )
             embedding_vector = response.data[0]["embedding"]
@@ -122,7 +139,7 @@ class BedrockPredictorEmbeddings(BedrockPredictor):
         return FMBenchPredictionResponse(response_json=self.response_json, latency=latency, completion_tokens=completion_tokens, prompt_tokens=prompt_tokens)
 
 def create_predictor(endpoint_name: str, inference_spec: Dict | None):
-    if endpoint_name in EMBEDDING_MODELS: ## handling for embedding models
+    if endpoint_name in global_vars['EMBEDDING_MODELS']: 
         return BedrockPredictorEmbeddings(endpoint_name, inference_spec) 
-    else: ## handling for all text generation models
+    else:
         return BedrockPredictor(endpoint_name, inference_spec)
