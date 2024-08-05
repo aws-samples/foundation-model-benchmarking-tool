2f41e5f5c336f2aa35bcb34a59c92f335852ac88
annn <43212517+antara678@users.noreply.github.com>
2024-08-05 17:56:28 -0500

    Delete src/fmbench/scripts directory

--- src/fmbench/scripts/compile-llm-for-aws-silicon/smep-with-lmi/package-inf2.sh
+++
@@ -1,5 +0,0 @@
-mkdir mymodel
-cp serving-inf2.properties  mymodel/serving.properties
-tar czvf mymodel-inf2.tar.gz mymodel/
-rm -rf mymodel
-aws s3 cp mymodel-inf2.tar.gz s3://llm-models/lmi/Meta-Llama-3-8B-Instruct/code/
