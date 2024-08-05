fc3127fc7426293acf2ae97d45a5f3752582daa6
annn <43212517+antara678@users.noreply.github.com>
2024-08-05 18:12:29 -0500

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -5,21 +5,19 @@
 
 CONDA_ENV_NAME=fmbench_python311
 CONDA_ENV_PATH=~/anaconda3/envs/$CONDA_ENV_NAME/lib/python3.11/site-packages
-CONFIG_FILE_PATH=src/fmbench/configs/llama3/8b/config-ec2-neuron-llama3-8b-inf2-24xl.yml
+CONFIG_FILE_PATH=src/fmbench/configs/llama2/7b/config-llama2-7b-g5-quick.yml
 #src/fmbench/configs/llama3/8b/config-llama3-8b-g5-streaming.yml
 #config-bedrock-llama3-streaming.yml #config-llama3-8b-g5-stream.yml
 LOGFILE=fmbench.log
 
-pip uninstall fmbench -y
 # delete existing install
 rm -rf $CONDA_ENV_PATH/fmbench*
 
 # build a new version
-poetry build -vvv
-
+poetry build
 pip install -U dist/*.whl
 
 # run the newly installed version
 echo "going to run fmbench now"
-fmbench --config-file $CONFIG_FILE_PATH --local-mode yes --write-bucket abstaticwebsitetest1 > $LOGFILE 2>&1
+fmbench --config-file $CONFIG_FILE_PATH  > $LOGFILE 2>&1
 echo "all done"
