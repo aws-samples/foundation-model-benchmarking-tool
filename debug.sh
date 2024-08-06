45673033d6fcd35c0ffb96f17df80ba8c3d6cc07
Ubuntu <ubuntu@ip-172-31-36-241.ec2.internal>
2024-08-06 21:09:44 +0000

    updated config

--- debug.sh
+++ debug.sh
@@ -5,19 +5,21 @@
 
 CONDA_ENV_NAME=fmbench_python311
 CONDA_ENV_PATH=~/anaconda3/envs/$CONDA_ENV_NAME/lib/python3.11/site-packages
-CONFIG_FILE_PATH=src/fmbench/configs/llama2/7b/config-llama2-7b-g5-quick.yml
+CONFIG_FILE_PATH=src/fmbench/configs/llama3/8b/config-ec2-neuron-llama3-8b-inf2-48xl.yml
 #src/fmbench/configs/llama3/8b/config-llama3-8b-g5-streaming.yml
 #config-bedrock-llama3-streaming.yml #config-llama3-8b-g5-stream.yml
 LOGFILE=fmbench.log
 
+pip uninstall fmbench -y
 # delete existing install
 rm -rf $CONDA_ENV_PATH/fmbench*
 
 # build a new version
-poetry build
+poetry build -vvv
+
 pip install -U dist/*.whl
 
 # run the newly installed version
 echo "going to run fmbench now"
-fmbench --config-file $CONFIG_FILE_PATH  > $LOGFILE 2>&1
+fmbench --config-file $CONFIG_FILE_PATH --local-mode yes --write-bucket madhur-llama3-fmbench-local-mode > $LOGFILE 2>&1
 echo "all done"
--- src/fmbench/scripts/neuron_deploy.py
+++ src/fmbench/scripts/neuron_deploy.py
@@ -8,14 +8,29 @@ Deploys a model on AWS silicon
 # Import necessary libraries
 import os
 import sys
+import stat
 import logging
 import subprocess
 from typing import Dict
 from pathlib import Path
 
+
 # Set up a logger
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+# Set up a separate logger for script output
+script_logger = logging.getLogger('script_logger')
+script_logger.setLevel(logging.INFO)
+
+# Create a file handler for logging script output in real time
+file_handler = logging.FileHandler('scripts.log', mode='a')
+file_handler.setLevel(logging.INFO)
+
+# Create a logging format and add the handler to the script logger
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+file_handler.setFormatter(formatter)
+script_logger.addHandler(file_handler)
 
 # Global constant for the Hugging Face token file
 SCRIPT_DIRECTORY: str = os.path.dirname(os.path.realpath(__file__))
@@ -23,20 +38,30 @@ HF_TOKEN_FNAME: str = os.path.join(SCRIPT_DIRECTORY, "hf_token.txt")
 neuron_script_dir: str = os.path.join(SCRIPT_DIRECTORY, "compile-llm-for-aws-silicon")
 shell_script_path: str = os.path.join(neuron_script_dir, "scripts/download_compile_deploy.sh")
 
+def make_executable(file_path: str) -> None:
+    """Make the script executable if it is not already."""
+    st = os.stat(file_path)
+    os.chmod(file_path, st.st_mode | stat.S_IEXEC)
+    logger.info("Made script executable: %s", file_path)
+
 def deploy(experiment_config: Dict, role_arn: str) -> Dict:
     """
     Function to deploy the model and create the endpoint
     """
     logger.info("Inside neuron deploy function")
+
+    # Ensure the script is executable
+    make_executable(shell_script_path)
     
     requirements_file_path = os.path.join(neuron_script_dir, "requirements.txt")
     command = [sys.executable, '-m', 'pip', 'install', '-r', requirements_file_path]
 
     try:
         subprocess.check_call(command)
-        print("Requirements installed successfully.")
+        logger.info("Requirements installed successfully.")
     except subprocess.CalledProcessError as e:
-        print(f"Error installing requirements: {e}")
+        logger.error(f"Error installing requirements: {e}")
+        raise
 
     try:
         model_id = experiment_config['model_id']
@@ -63,12 +88,14 @@ def deploy(experiment_config: Dict, role_arn: str) -> Dict:
         logger.info("Script Path: %s", neuron_script_dir)
         logger.info("Model Loading Timeout: %s", model_loading_timeout)
         logger.info("Initial Instance Count: %s", instance_count)
+
         hf_token_file_path = Path(HF_TOKEN_FNAME)
-        if hf_token_file_path.is_file() is True:
+        if hf_token_file_path.is_file():
             logger.info(f"hf_token file path: {hf_token_file_path} is a file")
-            HF_TOKEN = Path(HF_TOKEN_FNAME).read_text().strip()
+            HF_TOKEN = hf_token_file_path.read_text().strip()
         else:
-            logger.info(f"hf_token file path: {hf_token_file_path} is not a file")
+            logger.error(f"hf_token file path: {hf_token_file_path} is not a file")
+            raise FileNotFoundError(f"hf_token.txt file not found at {hf_token_file_path}")
         logger.info("HF Token is: %s", HF_TOKEN)
 
     except KeyError as e:
@@ -111,46 +138,54 @@ def deploy(experiment_config: Dict, role_arn: str) -> Dict:
     }
 
     try:
-        with open('scripts.log', 'a') as log_file:
-            result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            log_file.write("Standard Output:\n")
-            log_file.write(result.stdout)
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        
+        # Log output and errors in real time
+        for stdout_line in process.stdout:
+            script_logger.info(stdout_line.strip())
+        for stderr_line in process.stderr:
+            script_logger.error(stderr_line.strip())
+        
+        process.stdout.close()
+        process.stderr.close()
 
-            logger.info("Script executed successfully: %s", result.stdout)
-            logger.info("going to read endpoint from endpoint.txt")
+        return_code = process.wait()
+        if return_code != 0:
+            logger.error("Script failed with return code: %d", return_code)
+            raise subprocess.CalledProcessError(return_code, command)
 
-            try:
-                # Read the endpoint name from the endpoint.txt file
-                endpoint_file_path = os.path.join(neuron_script_dir, "endpoint.txt")
-                ep_name = Path(endpoint_file_path).read_text().strip()
-                logger.info("Endpoint is: %s", ep_name)
-                if ep_name:
-                    logger.info("Model endpoint running with name: %s", ep_name)
-                    deployment_result.update({
-                        'endpoint_name': ep_name,
-                        'experiment_name': experiment_config['name'],
-                        'instance_type': experiment_config['instance_type'],
-                        'instance_count': experiment_config['instance_count'],
-                        'deployed': True
-                    })
-                    logger.info("Deployment result: %s", deployment_result)
-                else:
-                    logger.error("Model endpoint not running!")
-            except FileNotFoundError:
-                logger.error("File endpoint.txt not found!")
-            except Exception as e:
-                logger.error("Error reading endpoint.txt: %s", str(e))
-                raise
+        logger.info("Script executed successfully, reading endpoint from endpoint.txt")
+
+        # Read the endpoint name from the endpoint.txt file
+        try:
+            endpoint_file_path = os.path.join(neuron_script_dir, "endpoint.txt")
+            ep_name = Path(endpoint_file_path).read_text().strip()
+            logger.info("Endpoint is: %s", ep_name)
+            if ep_name:
+                logger.info("Model endpoint running with name: %s", ep_name)
+                deployment_result.update({
+                    'endpoint_name': ep_name,
+                    'experiment_name': experiment_config['name'],
+                    'instance_type': experiment_config['instance_type'],
+                    'instance_count': experiment_config['instance_count'],
+                    'deployed': True
+                })
+                logger.info("Deployment result: %s", deployment_result)
+            else:
+                logger.error("Model endpoint not running!")
+        except FileNotFoundError:
+            logger.error("File endpoint.txt not found!")
+            raise
+        except Exception as e:
+            logger.error("Error reading endpoint.txt: %s", str(e))
+            raise
 
     except subprocess.CalledProcessError as e:
-        with open('scripts.log', 'a') as log_file:
-            log_file.write("Standard Error:\n")
-            log_file.write(e.stderr)
-        
-        logger.error("Error executing script: %s", e.stderr)
+        logger.error("Error executing script: %s", e)
         raise
     except Exception as e:
         logger.error("Unexpected error: %s", str(e))
         raise
+
     logger.info("Deployment result: %s", deployment_result)
     return deployment_result
