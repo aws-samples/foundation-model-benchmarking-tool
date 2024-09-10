90a2b2c0d1f65def00115e6c6b1b9cfc2c17deb3
Madhur Prashant <129979633+madhurprash@users.noreply.github.com>
2024-09-10 17:07:44 -0400

    Update triton-transformers-neuronx.sh

--- src/fmbench/scripts/triton/triton-transformers-neuronx.sh
+++ src/fmbench/scripts/triton/triton-transformers-neuronx.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 [ ! -d /triton ] && echo "/triton dir must exist" && exit 1
-[ $# -ne 2 ] && echo "usage: $0 hf-model-id model-name" && exit 1
+[ $# -ne 3 ] && echo "usage: $0 hf-model-id model-name http-port" && exit 1
 
+# The HF model id, model name and http port
+# are parsed from the triton multi model preparation
+# function here
 HF_MODEL_ID=$1
 MODEL_NAME=$2
+HTTP_PORT=$3
 
 LOG_ROOT=/triton/logs
 MODEL_REPO=/triton/model_repository
@@ -15,7 +19,8 @@ rm -rf $MODEL_REPO
 mkdir -p $MODEL_REPO
 VERSION=1
 mkdir -p $MODEL_REPO/$MODEL_NAME/$VERSION
-# Set permissions for directories (read, write, and execute)
+
+# Copy the model repository files into the triton container
 cp /triton/model.py $MODEL_REPO/$MODEL_NAME/$VERSION/model.py
 cp /triton/model.json $MODEL_REPO/$MODEL_NAME/$VERSION/model.json
 cp /triton/config.pbtxt $MODEL_REPO/$MODEL_NAME/config.pbtxt
@@ -24,9 +29,7 @@ export NEURON_CC_FLAGS="--model-type transformer"
 export NEURON_COMPILE_CACHE_URL="$CACHE_DIR"
 tritonserver \
 --model-repository=${MODEL_REPO} \
---grpc-port=8001 \
---http-port=8000 \
---metrics-port=8002 \
+--http-port=$HTTP_PORT \
 --disable-auto-complete-config \
 --log-file=$OUTPUT_LOG \
 && /bin/bash -c "trap : TERM INT; sleep infinity & wait"
