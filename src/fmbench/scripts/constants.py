e7fb577c4b3a5a67da311581d3dad482e6b11f40
Madhur Prashant <129979633+madhurprash@users.noreply.github.com>
2024-09-10 17:08:26 -0400

    Update constants.py

--- src/fmbench/scripts/constants.py
+++ src/fmbench/scripts/constants.py
@@ -4,6 +4,7 @@ from typing import List
 CONTAINER_TYPE_DJL: str = 'djl'
 CONTAINER_TYPE_VLLM: str = 'vllm'
 CONTAINER_TYPE_TRITON: str = 'triton'
+TRITON_INFERENCE_SCRIPT: str = '/scripts/triton/triton-transformers-neuronx.sh'
 
 AWS_CHIPS_PREFIX_LIST: List[str] = ["inf2", "trn1"]
 IS_NEURON_INSTANCE = lambda instance_type: any([instance_type.startswith(p) for p in AWS_CHIPS_PREFIX_LIST])
