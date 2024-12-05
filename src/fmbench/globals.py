2416f539f5b2e0e64cd01813d00fd5dfb205450d
Dheeraj Oruganty <53569374+dheerajoruganty@users.noreply.github.com>
2024-12-05 17:50:23 -0500

    Update globals.py

--- src/fmbench/globals.py
+++ src/fmbench/globals.py
@@ -26,7 +26,7 @@ current_working_directory: str = Path.cwd()
 
 CONFIG_FILEPATH_FILE: str = current_working_directory / 'config_filepath.txt'
 
-PRICING_FALLBACK_YAML_PATH="https://github.com/aws-samples/foundation-model-benchmarking-tool/blob/main/src/fmbench/configs/pricing.yml"
+PRICING_FALLBACK_YAML_PATH="https://raw.githubusercontent.com/aws-samples/foundation-model-benchmarking-tool/refs/heads/main/src/fmbench/configs/pricing_fallback.yml"
 
 # S3 client initialization
 s3_client = boto3.client('s3')
