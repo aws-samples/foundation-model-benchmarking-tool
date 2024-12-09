f9a875de0c862e0741952745ae305907a126053c
Ubuntu <ubuntu@ip-172-31-40-12.ec2.internal>
2024-12-09 14:46:35 +0000

    adding support for nova + openai + gemini

--- debug.sh
+++ debug.sh
@@ -38,7 +38,7 @@ CONFIG_FILE_PATH=src/fmbench/configs/external/config-nova-openai-gemini-models.y
 LOGFILE=fmbench.log
 
 # delete existing install
-rm -rf $CONDA_ENV_PATH/fmbench*
+# rm -rf $CONDA_ENV_PATH/fmbench*
 
 # build a new version
 poetry build
@@ -46,7 +46,7 @@ pip install -U dist/*.whl
 
 # run the newly installed version
 echo "going to run fmbench now"
-fmbench --config-file $CONFIG_FILE_PATH > $LOGFILE 2>&1
+fmbench --config-file $CONFIG_FILE_PATH  --local-mode yes --write-bucket placeholder --tmp-dir /tmp> $LOGFILE 2>&1
 
 # Use FMBench to benchmark models on hosted on EC2 using the command below. If you want to write the metrics and results to an
 # s3 bucket, replace `placeholder` with the name of that s3 bucket in your AWS account. Optionally, you can send the results to
