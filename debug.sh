d5c188d2dc0470ce654e77d1a3430d7731174fa1
Ubuntu <ubuntu@ip-172-31-45-70.ec2.internal>
2025-01-10 15:08:47 +0000

    refactoring the config more

--- debug.sh
+++ debug.sh
@@ -5,7 +5,7 @@
 
 CONDA_ENV_PATH=$CONDA_PREFIX/lib/python3.11/site-packages
 CONFIG_FILE_PATH=src/fmbench/configs/bedrock/config-bedrock-llama3-1.yml
-CONFIG_FILE_PATH=src/fmbench/configs/llama3.1/8b/config-llama3.1-8b-g5-version.yml
+CONFIG_FILE_PATH=src/fmbench/configs/generic/ec2/djl.yml
 #src/fmbench/configs/llama3.1/8b/config-llama3.1-8b-g5-ec2.yml
 #src/fmbench/configs/multimodal/bedrock/config-llama-3-2-11b-vision-instruct-scienceqa.yml
 #src/fmbench/configs/multimodal/bedrock/config-llama-3-2-11b-vision-instruct-image-dataset.yml
@@ -48,7 +48,7 @@ pip install -U dist/*.whl
 
 # run the newly installed version
 echo "going to run fmbench now"
-fmbench --config-file $CONFIG_FILE_PATH  --local-mode yes --write-bucket placeholder --tmp-dir /tmp -A model_id=meta-llama/Llama-3.1-8B-Instruct -A instance_type=g5.4xlarge -A tp_degree=1 -A batch_size=4 > $LOGFILE 2>&1
+fmbench --config-file $CONFIG_FILE_PATH  --local-mode yes --write-bucket placeholder --tmp-dir /tmp -A model_id=meta-llama/Llama-3.1-8B-Instruct -A instance_type=g5.4xlarge -A tp_degree=1 -A batch_size=4 -A results_dir=llama3.1-8b-g5-djl > $LOGFILE 2>&1
 
 # Use FMBench to benchmark models on hosted on EC2 using the command below. If you want to write the metrics and results to an
 # s3 bucket, replace `placeholder` with the name of that s3 bucket in your AWS account. Optionally, you can send the results to
