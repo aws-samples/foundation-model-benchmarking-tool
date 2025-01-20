d30ab7829be61b3ac5d26ad2d4fc0c805789215a
dheerajoruganty <db2winfb@gmail.com>
2025-01-20 23:38:33 +0000

    add Ollama deployment script

---
+++ src/fmbench/scripts/inference_containers/ollama.py
@@ -0,0 +1,40 @@
+"""
+ollama specific code
+"""
+import logging
+from fmbench.scripts.inference_containers.utils import (STOP_AND_RM_CONTAINER,
+                                                        FMBENCH_MODEL_CONTAINER_NAME)
+
+logging.basicConfig(format='[%(asctime)s] p%(process)s {%(filename)s:%(lineno)d} %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def create_script(region, image_uri, model_id, model_name, env_str, privileged_str, hf_token, directory):
+    """
+    Script for running the docker container for the inference server
+    """
+    script = f"""#!/bin/sh
+
+        {STOP_AND_RM_CONTAINER}
+
+        #Stop ollama
+        systemctl stop ollama.service
+
+        # Pull the specified model using Ollama
+        echo "Pulling model: {model_id}..."
+        ollama pull {model_id}
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to pull model {model_id}."
+            exit 1
+        fi
+
+        # Serve the specified model using Ollama
+        echo "Starting to serve model: {model_id}..."
+        ollama serve {model_id} &
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to serve model {model_id}."
+            exit 1
+        fi
+
+        echo "Successfully serving model_id = {model_id} with Ollama."
+    """
+    return script
